ce793b2ec3e06df3d3be676911fe85c59ed2531e
po3rin <abctail30@gmail.com>
2020-06-03 17:03:26 +0900

    error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,8 +2,18 @@
   <div class="container">
     <h1>{{ error.statusCode }}</h1>
     <h1>😢</h1>
-    <p v-if="error.statusCode === 404">ページが見つかりません</p>
-    <p v-else>エラーが発生しました</p>
+    <div v-if="error.statusCode === 404" class="container_item">
+      <p class="message">ページが見つかりません</p>
+    </div>
+    <div v-else class="container_item">
+      <p class="message">一時的に障害が起きているようです。</p>
+      <p class="description">
+        現在復旧作業を行なっています。ご迷惑をおかけして申し訳ございません。記事のマスターはGitHub上でご覧いただけます。<br />
+        <a href="https://github.com/po3rin/post/blob/master/CONTENTS.md"
+          >記事のリポジトリへ</a
+        >
+      </p>
+    </div>
   </div>
 </template>
 
@@ -19,12 +29,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+p {
+  line-height: 2em;
+  vertical-align: baseline;
+  font-weight: 300;
+}
+
 .container {
   min-height: 50vh;
-  padding: 64px;
+  padding: 48px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  &_item {
+    max-width: 512px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+.message {
+  font-weight: bold;
+  font-size: 1rem;
+}
+
+.description {
+  font-size: 0.9rem;
 }
 </style>
